4a8d5996f67047bfab8505934acc0cec56abb096
Zakaria Hamichi <Hamichi.zakaria@gmail.com>
2018-11-17 11:28:52 +0100

    Eyecherry

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,38 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+    
+    public void takeOff() {
+    	System.out.println(getName() +" Takes off in 3, 2, 1 ..");
+    }
+    
+    public void land() {
+    	System.out.println("Prepare for crash landing, brace for impact");
+    }
+    
+    public void ascend(int meters) {
+    	System.out.println("He flies up "+ meters + " meters into the air !");
+    }
+    
+    public void descend(int meters) {
+		System.out.println(getName() + " comes shooting down " + meters + " meters !!");
+    }
+    
+    public void glide() {
+		System.out.println("He glides majestically into the air");
+	}
+    
+    public void ascend2(int meters) {
+    	System.out.println(getName() + " continues his ascent another " + meters + " meters");
+    }
+    
+    public void descend2(int meters) {
+    	System.out.println(getName() + " slows down , he comes down another " + meters + " meters");
+    	
+    }
+    
+    public void land2() {
+    	System.out.println(getName() + " softly lands on a tree branch, oklm");
+    }
 }
+    
